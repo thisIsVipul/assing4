712c2fdff85a7187aa0f4940970a73bd05e9eaa5
thisIsVipul <71059403+thisIsVipul@users.noreply.github.com>
2021-06-12 09:12:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="dhoni";
+names[2]="jholi";
+names[3]="jaina";
+names[4]="yuvi";
+names[5]="jadu";
+names[6]="rahul";
+names[7]="gayel";
+names[8]="kumar";
+names[9]="jim";
+
+for (var i = 0; i < names.length; i++) {
+    if(names[i].charAt(0)==='j'|| names[i].charAt(0)==='j'){
+console.log("Goodbye "+ names[i])
+    }
+    else{
+        console.log("hello"+names[i])
+    }
+   
+}
